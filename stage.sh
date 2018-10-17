56dd71e8595307078cbd131cc5fca08320cc9a1c
jeremy <jtnuttal@usc.edu>
2018-10-16 23:10:06 -0700

    Pre-publish commit

--- stage.sh
+++ stage.sh
@@ -15,14 +15,14 @@ git checkout staging
 # merge master into staging
 git merge master
 # build the Ember application
-cd $FRONTEND && ember build --environment staging && cd ..;
+cd $FRONTEND && ember build --environment staging && cd -;
 # clear public
 rm -Rf $BACKEND/public/assets;
 rm -Rf $BACKEND/public/fonts;
 rm -Rf $BACKEND/public/index.html;
 rm -Rf $BACKEND/public/crossdomain.xml;
 # copy the new Ember build into the Laravel application
-cp -R $FORNTEND/dist/assets $BACKEND/public
+cp -R $FRONTEND/dist/assets $BACKEND/public
 cp -R $FRONTEND/dist/fonts $BACKEND/public
 cp -R $FRONTEND/dist/index.html $BACKEND/resources/views/index.blade.php
 cp -R $FRONTEND/dist/crossdomain.xml $BACKEND/public
